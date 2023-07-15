b4285ca8fec05b2cf40c0f48bf0604c8d537a1ce
Majid Khan <105569780+Majid1k@users.noreply.github.com>
2023-07-15 13:09:22 +0200

    build a responsive layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
-import { Button } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react"; // see chakra-ui web => grid section
 
 function App() {
   return (
     <>
-      <Button colorScheme="blue">Button</Button>
+      <Grid
+        // we pass an object in templateArea to set responsiveness for different screens
+        templateAreas={{
+          base: `"nav" "main"`, // in base size screen we want to show only navbar & main area
+          lg: `"nav nav" "aside main"`, // but on large screens(wider then 1024px) we show nav on full top area & aside and main area on bottom
+        }}
+      >
+        <GridItem pl="2" bg="orange.300" area={"nav"}>
+          Nav
+        </GridItem>
+
+        <Show above="lg">
+          {" "}
+          {/*Show/Hide component chakra-ui, conditional renders if screen size is above/below to the given size , give screen size as props*/}
+          <GridItem pl="2" bg="gray.300" area={"aside"}>
+            Aside
+          </GridItem>
+        </Show>
+
+        <GridItem pl="2" bg="green.300" area={"main"}>
+          Main
+        </GridItem>
+      </Grid>
     </>
   );
 }
