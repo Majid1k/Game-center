732fd34baf8ffd9fc1bf1430cf95d1e919c93253
Majid Khan <105569780+Majid1k@users.noreply.github.com>
2023-07-15 16:13:14 +0200

    Implement Dark Mode

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import App from "./App.jsx";
+// 4 DarkMode- import theme dark switch, 5- set the theme={theme} in chakra provider below & import & use ColorModeScript as given below,
+// after that inspect => application => local storage, you'll see light mode, delete it and refresh page, now page is dark mode coz chakra save this setting in local storage
+import theme from "./theme.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <App />
     </ChakraProvider>
   </React.StrictMode>
---
+++ src/theme.js
@@ -0,0 +1,31 @@
+// Chakra-Ui => Color MOde for dark mode theme
+
+import { extendTheme } from "@chakra-ui/react"; // 1- import extend theme
+
+const config = {
+  // 2- create an object for initial color property
+  initialColorMode: "dark",
+};
+
+// 3- call extendTheme function and pass config object in it
+const theme = extendTheme({
+  config,
+});
+
+// 4- import and use in main.jsx
+export default theme;
+
+/*
+colors: {
+  gray: {
+    50: "#f9f9f9",
+    100: "#ededed",
+    200: "#d3d3d3",
+    300: "#b3b3b3",
+    400: "#a0a0a0",
+    500: "#898989",
+    600: "#6c6c6c",
+    700: "#202020",
+    800: "#121212",
+    900: "#111",
+  }, */
