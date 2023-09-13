6e6109f837d00149af58eb50fe82a46e34adcdaf
Majid Khan <105569780+Majid1k@users.noreply.github.com>
2023-09-14 01:20:33 +0200

    Refectoring- creating reusable APIClient

--- src/hooks/useGenres.js
+++ src/hooks/useGenres.js
@@ -1,12 +1,14 @@
 import genres from "../data/genres";
+import APIClient from "../services/api-client";
 
 import { useQuery } from "@tanstack/react-query";
-import apiClient from "../services/api-client";
+
+const apiClient = new APIClient("/genres");
 
 const useGenres = () =>
   useQuery({
     queryKey: ["genres"],
-    queryFn: () => apiClient.get("/genres").then((res) => res.data),
+    queryFn: apiClient.getAll,
     staleTime: 24 * 60 * 60 * 1000,
     initialData: { count: genres.length, results: genres },
   });
--- src/services/api-client.js
+++ src/services/api-client.js
@@ -1,11 +1,20 @@
-// custom api
-import axios, { CanceledError } from "axios";
+import axios from "axios";
 
-export default axios.create({
+const axiosInstance = axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
     key: "c1932d10ef6b43ff82ce302da395e467",
   },
 });
 
-export { CanceledError };
+class APIClient {
+  constructor(endpoint) {
+    this.endpoint = endpoint;
+  }
+
+  getAll = (config) => {
+    return axiosInstance.get(this.endpoint, config).then((res) => res.data);
+  };
+}
+
+export default APIClient;
